64e74cfc713546cb78a50aa7fa475a8e6854a903
Vaxee03 <012jas84@gmail.com>
2024-11-29 07:36:41 +0900

    레벨 2-1 : ExchangeController 생성
    ExchangeController Class 생성

---
+++ src/main/java/com/sparta/currency_user/controller/ExchangeController.java
@@ -0,0 +1,19 @@
+package com.sparta.currency_user.controller;
+
+import com.sparta.currency_user.dto.ExchangeResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exchange")
+@RequiredArgsConstructor
+public class ExchangeController {
+
+}
